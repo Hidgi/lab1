0dd1fe3e622fb7f458bdc6abb548a276c72c15f6
Hidgi <59579597+Hidgi@users.noreply.github.com>
2020-12-17 16:40:04 +0300

    Разбил код на функции, добавил задание "а", вывел а, b, c.

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -10,8 +10,42 @@ def division(a, g):
         a = ''.join(a2)
         while a[0] == '0' and len(a) > 1:
             a = a[1:]
-    print('Result of division', c, ' by ', g, ' is ', a)
     return a
+
+def enc (g, a, e):
+    b_enc = ''
+    c = i
+    r_enc = len(g) - 1
+    a_enc = division(i, g)
+    print('c =', a_enc)
+    a_enc = '0' * (r_enc - len(a_enc)) + a_enc
+    c = c[:-r_enc] + a_enc
+    print('a =', c)
+    e = '0' * (len(c) - len(e)) + e
+    print('e =', e)
+    for j in range(len(c)):
+        b_enc = b_enc + str((int(c[j]) + int(e[j])) % 2)
+    print('b =', b_enc)
+    return b_enc
+
+def check(b, g):
+    b_check = division(i, g)
+    print('При делении ', b, ' на ', g, ' остаток ', b_check)
+    if b_check == '' or b_check == '0':
+        s = 0
+    else:
+        s = 1
+    return s
+
+def check2(b, g, r):
+    a_check = b[:-r]
+    a_check = a_check + '0' * (r)
+    a_check = division(a, g)
+    if a_check == b[-r:]:
+        s1 = 0
+    else:
+        s1 = 1
+    return s1
 
 g = input('Введите вектор g(x): ')
 k = int(input('Введите k: '))
@@ -22,7 +56,11 @@ m = [0]
 a = []
 b = []
 s = []
-if len(l) <= k:
+s1 = []
+if len(l) < k:
+    l = '0'*(k-len(l))+l
+    m[0] = l
+elif len(l) == len(k):
     m[0] = l
 else:
     m[0] = l[:k]
@@ -35,26 +73,19 @@ kks = '0' * r
 for i in m:
     a.append(i + kks)
 for i in a:
-    b_enc = ''
-    c = i
-    r_enc = len(g) - 1
-    a_enc = division(i, g)
-    a_enc = '0' * (r_enc - len(a_enc)) + a_enc
-    c = c[:-r_enc] + a_enc
-    e = '0' * (len(c) - len(e)) + e
-    for j in range(len(c)):
-        b_enc = b_enc + str((int(c[j]) + int(e[j])) % 2)
- 
-        b.append(b_enc)
+    b.append(enc(g, i, e))
 for i in b:
-    b_check = division(i, g)
-    if b_check == '' or b_check == '0':
-        res = 0
-    else:
-        res = 1
-    s.append(res)
+     s.append(check(i, g))
+     s1.append(check2(i,g,r))
 for i in range(len(s)):
+    print('Проверка № 1')
     if s[i] == 0:
+        print('В ', i + 1, ' части кода ошибок не обнаружено')
+    else:
+        print('В ', i + 1, ' части кода ошибка обнаружена')
+for i in range(len(s)):
+    print('Проверка № 2')
+    if s1[i] == 0:
         print('В ', i + 1, ' части кода ошибок не обнаружено')
     else:
         print('В ', i + 1, ' части кода ошибка обнаружена')
